8e72810ae5bc5406de3296db43d15aed5d7a5b61
hasan-ullah <mdhasanullah124@gmail.com>
2022-02-22 08:55:18 +0600

    update phone and case button working

--- app-2.js
+++ app-2.js
@@ -1,1 +1,11 @@
-console.log('js added')
+
+
+// updat mobile  
+document.getElementById('phone-plus').addEventListener('click', function(){
+    console.log('okk')
+ })
+
+ // updat mobile casing 
+document.getElementById('case-plus').addEventListener('click', function(){
+    console.log('okk')
+ })
